3b8cc39767cf1afb816ffda279b2642295fee075
AndyGrigs <andygrigs88@gmail.com>
2023-07-05 17:39:42 +0200

    sccs module

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,13 @@
 
 import React, { useState } from "react";
-import '../style.scss'
+import cl from './Counter.module.scss'
 
 export function Counter() {
   const [count, setCount] = useState(0);
   return (
     <div>
      <h1>{count}</h1>
-      <button onClick={()=> setCount(count + 1)}>Counter</button>
+      <button className={cl.button} onClick={()=> setCount(count + 1)}>Counter</button>
     </div>
   );
 }
