80e3aefa5d9f2575bd929cd461f4c26b5525513b
AndyGrigs <andygrigs88@gmail.com>
2023-07-05 14:22:48 +0200

    adding css file and webpack css loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Counter } from './components/Counter';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <div>hello</div>    
+        <Counter/>  
     </React.StrictMode>
 );
