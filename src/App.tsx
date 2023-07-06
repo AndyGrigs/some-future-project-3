1dd4b4d81010902ad62dd59f12e5f59fd4a13ce1
AndyGrigs <andygrigs88@gmail.com>
2023-07-06 09:20:52 +0200

    adding router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './index.scss'
-import { Counter } from './components/Counter'
+import { Route, Routes } from 'react-router-dom'
+import AboutPage from './pages/AboutPage/AboutPage'
+import MainPage from './pages/MainPage/MainPage'
+import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
-        <div className="patience">
-            <p>Patience is the ability to wait calmly in the face of frustration or adversity.</p>
-        </div>
-        <Counter/>
+      <Link to='/about'>About</Link>
+      <Link to='/'>Home</Link>
+        <Routes>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/' element={<MainPage/>}/>
+        </Routes>
     </div>
   )
 }
