4d3d65018e0ca1794f35e680aca4a9a56f79de13
AndyGrigs <andygrigs88@gmail.com>
2023-07-07 09:14:56 +0200

    adding suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import './index.scss'
 import { Route, Routes } from 'react-router-dom'
-import AboutPage from './pages/AboutPage/AboutPage'
-import MainPage from './pages/MainPage/MainPage'
 import { Link } from 'react-router-dom'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+
 
 const App = () => {
   return (
     <div className='app'>
-      <Link to='/about'>About</Link>
+      <Link to='/about'>About</Link><br />
       <Link to='/'>Home</Link>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPage/>}/>
-          <Route path='/' element={<MainPage/>}/>
+          <Route path='/about' element={<AboutPageAsync/>}/>
+          <Route path='/' element={<MainPageAsync/>}/>
         </Routes>
+      </Suspense>
     </div>
   )
 }
