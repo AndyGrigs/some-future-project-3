4e595be10659d4e44de11b8a8d729f89e9e1a1a6
AndyGrigs <andygrigs88@gmail.com>
2023-07-09 00:04:18 +0200

    adding toogling themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React, {Suspense} from 'react'
-import './index.scss'
+import React, {Suspense, useState} from 'react'
+import './styles/index.scss'
 import { Route, Routes } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
 
 const App = () => {
+
+
+    const[theme, setTheme] = useState<Theme>(Theme.LIGHT)
+    
+    const toogleTheme = () =>{
+      setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+    }
+
+
   return (
-    <div className='app'>
+    <div className= {`app ${theme}`}>
+      <button onClick={toogleTheme}>toogle</button>
       <Link to='/about'>About</Link><br />
       <Link to='/'>Home</Link>
       <Suspense fallback={<div>Loading...</div>}>
