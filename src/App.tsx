f0fdc8a9c3fbe09a86bd08c0769f8acc44898cc0
Andriy <andygrigs88@gmail.com>
2024-03-23 01:18:17 +0200

    adding useTheme hook, className func

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import React, {Suspense, useState} from 'react'
+import React, { Suspense, useContext, useState } from 'react'
 import './styles/index.scss'
 import { Route, Routes } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { Theme, ThemeContext } from './theme/ThemeContext'
+import { useTheme } from './theme/useTheme'
+import { classNames } from './shared/helpers/classNames'
 
-export enum Theme {
-  LIGHT = 'light',
-  DARK = 'dark',
-}
 
 const App = () => {
 
-
-    const[theme, setTheme] = useState<Theme>(Theme.LIGHT)
-    
-    const toogleTheme = () =>{
-      setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
-    }
-
-
+  const { theme, toogleTheme } = useTheme()
   return (
-    <div className= {`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toogleTheme}>toogle</button>
       <Link to='/about'>About</Link><br />
       <Link to='/'>Home</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPageAsync/>}/>
-          <Route path='/' element={<MainPageAsync/>}/>
+          <Route path='/about' element={<AboutPageAsync />} />
+          <Route path='/' element={<MainPageAsync />} />
         </Routes>
       </Suspense>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ThemeProvider from './theme/ThemeProvider';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-        <App/>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
